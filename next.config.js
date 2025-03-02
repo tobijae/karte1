47370fb0339304b6a4541557d6c041ebcca77335
Tobiasfib <72310273+tobijae@users.noreply.github.com>
2025-03-02 15:27:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  devIndicators: false
+  devIndicators: false,
+  output: 'export',
+  basePath: '/karte1',
+  images: {
+    unoptimized: true,
+  }
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
